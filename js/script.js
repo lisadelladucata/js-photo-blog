6056449ce8b425dc2b344b929685e1fa190cbbbc
lisadelladucata <lisa.delladucata@gmail.com>
2025-01-09 18:38:52 +0100

    feat:add-img&info

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 // DOM ELEMENTS 
-const imgElm = document.querySelector('.img')
-const infoElm = document.querySelector('.info')
+const imgElm = document.querySelectorAll('.img')
+const infoElm = document.querySelectorAll('.info')
+
+console.log(infoElm , imgElm)
 
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
   .then(function (response) {
     console.log(response.data)
+    let counter = 0
     const info = response.data
     info.forEach(function (info) {
-    infoElm.innerHTML += `<div class="info">
-                            <p>${info.title}</p>
-                        </div>`;
+    infoElm[counter].innerHTML = `<div class="info">
+                                    <p>${info.title}</p>
+                                </div>`;
+    imgElm[counter].innerHTML = `<div class="img">
+                                    <img src="${info.url}" alt="prova">
+                                </div>`;
+    counter++;
   });
   });
